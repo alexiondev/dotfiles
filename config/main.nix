280da13b38e9ab54ac3ee23c419fb2a5d572ba80
Alexion Ramos <ratex98@gmail.com>
2021-12-25 11:33:17 -0500

    Added XDG directories.

--- config/main.nix
+++ config/main.nix
@@ -18,5 +18,22 @@
     picom.enable = true;
   };
 
+  xdg = {
+    enable = true;
+    userDirs = {
+      enable = true;
+      createDirectories = true;
+
+      desktop     = "$HOME/.desktop";
+      documents   = "$HOME/doc";
+      download    = "$HOME/dwn";
+      music       = "$HOME/.hideme";
+      pictures    = "$HOME/pic";
+      publicShare = "$HOME/.hideme";
+      templates   = "$HOME/.hideme";
+      videos      = "$HOME/.hideme";
+    };
+  };
+  
   xsession.enable = true;
 }
