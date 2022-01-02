eef67ab675ede20c4cd0e48c15b516461d2b12ed
Alexion Ramos <ratex98@gmail.com>
2022-01-02 01:54:14 -0500

    Changed some VSCode settings using Nix.

--- modules/editors/vscode.nix
+++ modules/editors/vscode.nix
@@ -14,8 +14,28 @@ in {
         fonts = config.modules.theme.fonts.default.monospace ++ fa;
         fa = ["Font Awesome 5 Brands" "Font Awesome 5 Free" "Font Awesome 5 Free Solid"];
       in {
+        "editor.bracketPairColorization.enabled" = true;
+        "editor.guides.bracketPairs" = true;
+        "editor.copyWithSyntaxHighlighting" = false;
+        "editor.rulers" = [80 120];
+        "editor.smoothScrolling" = true;
         "editor.fontFamily" = lib.concatStringsSep "," (map (x: "'${x}'") fonts);
         "editor.fontLigatures" = true;
+        "editor.minimap.enabled" = false;
+        "files.enableTrash" = false;
+        "files.insertFinalNewline" = true;
+        "files.trimFinalNewlines" = true;
+        "files.trimTrailingWhitespace" = true;
+        "workbench.list.smoothScrolling" = true;
+        "workbench.panel.opensMaximized" = "never";
+        "workbench.startupEditor" = "none";
+        "workbench.editor.closeOnFileDelete" = true;
+        "workbench.editor.labelFormat" = "medium";
+        "workbench.settings.openDefaultKeybindings" = true;
+        "terminal.integrated.copyOnSelection" = true;
+        "terminal.integrated.enableBell" = true;
+        "keyboard.dispatch" = "keyCode";
+        "telemetry.telemetryLevel" = "off";
       };
     };
   };
