8ce0bed39a577a281f6c8a162118b7a72790f89f
Alexion Ramos <ratex98@gmail.com>
2022-01-07 06:39:42 -0500

    Added NUR

--- flake.nix
+++ flake.nix
@@ -12,26 +12,28 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     nixpkgs-unstable.url = "nixpkgs/nixpkgs-unstable";
+    nur.url = "github:nix-community/NUR";
 
     home-manager.url = "github:rycee/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs @ { self, nixpkgs, nixpkgs-unstable, ... }:
+  outputs = inputs @ { self, nixpkgs, nixpkgs-unstable, nur, ... }:
   let inherit (lib.my) loadHosts loadModules;
       lib = nixpkgs.lib.extend
         (self: super: {my = import ./lib.nix { inherit pkgs inputs; lib = self; };});
-      
+
       mkPkgs = pkgs: extraOverlays: import pkgs {
         system = "x86_64-linux";
         config.allowUnfree = true;
         overlays = extraOverlays;
       };
 
-      pkgs  = mkPkgs nixpkgs [ self.overlay ];
+      pkgs  = mkPkgs nixpkgs [ self.overlay nur.overlay ];
       pkgs' = mkPkgs nixpkgs-unstable [];
   in {
     lib = lib;
+    packs = pkgs;
 
     overlay = final: prev: {
       unstable = pkgs';
